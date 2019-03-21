e1eb071d54d5dea1179450c6885cccd54c695317
Christian Hess <christianhess.rlz@gmail.com>
2019-03-21 13:31:14 -0300

    Add Babel Polyfills to support old browsers and IE

--- shuup_product_reviews/static_src/index.js
+++ shuup_product_reviews/static_src/index.js
@@ -6,5 +6,6 @@
  * This source code is licensed under the OSL-3.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+require("babel-polyfill");
 require("./scripts.js");
 require("./styles.scss");
